e4414b5590c74c671836d17d18f2f9e480a6f810
zhenmunse <2711675247@qq.com>
2025-09-25 21:44:37 -0700

    Update HW1

---
+++ HW1/chatbot.py
@@ -0,0 +1,5 @@
+# Homework 1 Part B (chatbot.py)
+# Coder: Zonglin Han
+# This program prints a message of ECS32A chatbot.
+
+print("Hello user!\n\nI'm the ECS32A chatbot!\n") # print a simple message to the screen
---
+++ HW1/welcome.py
@@ -0,0 +1,5 @@
+# Homework 1 Part A (welcome.py)
+# Coder: Zonglin Han
+# This program prints a welcome message.
+
+print("Welcome to ECS32A!")   # print a simple message to the screen
